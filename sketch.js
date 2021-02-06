1618787f8265b48dce74641786d52d1da63fb15e
Maanas-K <76108537+Maanas-K@users.noreply.github.com>
2021-02-06 19:02:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,21 +21,24 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	
+	var options = {
+		isStatic: true
+	}
+	roof1 = Bodies.rectangle(400,200,600,10,options);
 
-	ball1 = new ball(300,300,50);
-	ball2 = new ball(350,300,50);
-	ball3 = new ball(400,300,50);
-	ball4 = new ball(450,300,50);
-	ball5 = new ball(500,300,50);
+	ball1 = new ball(300,500,50);
+	ball2 = new ball(350,500,50);
+	ball3 = new ball(400,500,50);
+	ball4 = new ball(450,500,50);
+	ball5 = new ball(500,500,50);
 
-	rope1=new Rope(roof1,ball1);
-	rope2=new Rope(roof1,ball2);
+	rope1=new Rope(ball1,ball2);
+	/*rope2=new Rope(roof1,ball2);
 	rope3=new Rope(roof1,ball3);
 	rope4=new Rope(roof1,ball4);
-	rope5=new Rope(roof1,ball5);
+	rope5=new Rope(roof1,ball5);*/
 
-	roof1=new Roof(400,200,600,50);
+	
 
 
 	
@@ -56,24 +59,25 @@ function draw() {
   ball4.display();
   ball5.display();
 
-  rope1.display();
-  rope2.display();
+  //rope1.display();
+ /* rope2.display();
   rope3.display();
   rope4.display();
-  rope5.display();
+  rope5.display();*/
 
   roof1.display();
 
 
 
   
-if(keyDown("space")){
+/*if(keyDown("space")){
 	Matter.Body.applyForce(ball1.body,{x: ball1.body.position.x, y: ball1.body.position.y},{x: -120,y: 0})
-  }	
+  }	*/
   
   drawSprites();
  
 }
+
 
 
 
